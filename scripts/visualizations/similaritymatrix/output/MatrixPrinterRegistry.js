b2bf4eb72f90c936ea4469c3e990bf721c7de3b6
Jefferey Cave <jefferey.cave@gmail.com>
2018-04-17 00:27:23 +0000

    Removed MatrixRegistry in favour of simple collection of functions

--- scripts/visualizations/similaritymatrix/output/MatrixPrinterRegistry.js
+++ scripts/visualizations/similaritymatrix/output/MatrixPrinterRegistry.js
@@ -1,51 +1,14 @@
-/*
- * CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").
- * You may not use this file except in compliance with the License.
- *
- * See LICENSE.txt included in this distribution for the specific
- * language governing permissions and limitations under the License.
- *
- * CDDL HEADER END
- *
- * Copyright (c) 2014-2015 Nicholas DeMarinis, Matthew Heon, and Dolan Murvihill
- */
 'use strict';
 export {
 	MatrixPrinterRegistry
 };
 
-import {RegistryWithDefault} from '../../../util/reflection/RegistryWithDefault.js';
-import {MatrixToHTMLPrinter} from '../../../visualizations/similaritymatrix/output/MatrixToHTMLPrinter.js';
 
 
 /**
  * Registry for Matrix Printers.
  */
-class MatrixPrinterRegistry extends RegistryWithDefault {
-	constructor() {
-		let printers = [
-				'MatrixToCSVPrinter',
-				'MatrixToHTMLPrinter',
-			];
-		let def = MatrixToHTMLPrinter.getInstance().getName();
-		super( printers, "MatrixPrinter", [], def);
-	}
-
-	/**
-	 * @return Singleton instance of MatrixPrinterRegistry
-	 */
-	static getInstance() {
-		if('instance' in MatrixPrinterRegistry) {
-			return MatrixPrinterRegistry.instance;
-		}
-		MatrixPrinterRegistry.instance = new MatrixPrinterRegistry();
-		return MatrixPrinterRegistry.instance;
-	}
-
-	toString() {
-		return "Singleton instance of MatrixPrinterRegistry";
-	}
-}
+const MatrixPrinterRegistry = {
+	"def":'matrixtohtmlprinter',
+	"processors":{}
+};
--- scripts/visualizations/similaritymatrix/output/MatrixToCSVPrinter.js
+++ scripts/visualizations/similaritymatrix/output/MatrixToCSVPrinter.js
@@ -1,106 +1,55 @@
-/*
- * CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").
- * You may not use this file except in compliance with the License.
- *
- * See LICENSE.txt included in this distribution for the specific
- * language governing permissions and limitations under the License.
- *
- * CDDL HEADER END
- *
- * Copyright (c) 2014-2015 Nicholas DeMarinis, Matthew Heon, and Dolan Murvihill
- */
 'use strict';
-export {
-	MatrixToCSVPrinter
-};
 
-import {MatrixPrinter} from '../../../visualizations/similaritymatrix/output/MatrixPrinter.js';
+import {MatrixPrinterRegistry} from '../../../visualizations/similaritymatrix/output/MatrixPrinterRegistry.js';
 import {checkNotNull} from '../../../util/misc.js';
 
+
+(function(){
+
 /**
- * Print a Similarity Matrix as machine-readable CSV.
+ * Print a Similarity Matrix in CSV format.
+ *
+ * @param matrix Matrix to print
+ * @return CSV representation of matrix
+ * @throws InternalAlgorithmError Thrown on internal error processing matrix
  */
-class MatrixToCSVPrinter extends MatrixPrinter {
-	constructor(){
-		if('instance' in MatrixToCSVPrinter){
-			throw Error('Meant to be a singleton. Use "getInstance"');
-		}
-		super();
+MatrixPrinterRegistry.processors['csv'] = function(matrix){
+	checkNotNull(matrix);
 
+	let builder = [];
 
-		MatrixToCSVPrinter.instance = this;
+	// First row: NULL, then all the Y submissions, comma-separated
+	let row = [null];
+	for(let y = 0; y < matrix.ySubmissions.length; y++) {
+		row.push(matrix.ySubmissions[y].Name);
 	}
+	builder.push(row);
 
-	/**
-	 * @return Singleton instance of MatrixToCSVPrinter
-	 */
-	static getInstance() {
-		if(!('instance' in MatrixToCSVPrinter)) {
-			MatrixToCSVPrinter.instance = new MatrixToCSVPrinter();
-		}
-		return MatrixToCSVPrinter.instance;
-	}
-
-	/**
-	 * Print a Similarity Matrix in CSV format.
-	 *
-	 * @param matrix Matrix to print
-	 * @return CSV representation of matrix
-	 * @throws InternalAlgorithmError Thrown on internal error processing matrix
-	 */
-	printMatrix(matrix){
-		checkNotNull(matrix);
-
-		let builder = [];
-
-		// First row: NULL, then all the Y submissions, comma-separated
-		let row = [null];
+	// Remaining rows: X label, then all Y results in order
+	for(let x = 0; x < matrix.xSubmissions.length; x++) {
+		// First, append name of the X submission
+		row = [matrix.xSubmissions[x].Name];
+		// Next, append all the matrix values, formatted as given
 		for(let y = 0; y < matrix.ySubmissions.length; y++) {
-			row.push(matrix.ySubmissions[y].Name);
+			let entry = matrix.getEntryFor(x, y);
+			let pct = entry.similarityPercent;
+			row.push(pct);
 		}
 		builder.push(row);
-
-		// Remaining rows: X label, then all Y results in order
-		for(let x = 0; x < matrix.xSubmissions.length; x++) {
-			// First, append name of the X submission
-			row = [matrix.xSubmissions[x].Name];
-			// Next, append all the matrix values, formatted as given
-			for(let y = 0; y < matrix.ySubmissions.length; y++) {
-				let entry = matrix.getEntryFor(x, y);
-				let pct = entry.similarityPercent;
-				row.push(pct);
-			}
-			builder.push(row);
-		}
-
-		builder = builder
-			.map(function(row){
-				return JSON.stringify(row)
-					.replace(/^\[/,'')
-					.replace(/]$/,'')
-					;
-			})
-			.join('\n')
-			;
-		return builder;
 	}
 
-	getName() {
-		return "csv";
-	}
+	builder = builder
+		.map(function(row){
+			return JSON.stringify(row)
+				.replace(/^\[/,'')
+				.replace(/]$/,'')
+				;
+		})
+		.join('\n')
+		;
+	return builder;
+};
 
-	toString() {
-		return "Singleton matrix of MatrixToCSVPrinter";
-	}
 
-	hashCode() {
-		return this.getName().hashCode();
-	}
 
-	equals(other) {
-		return other instanceof 'MatrixToCSVPrinter';
-	}
-}
+})();
