9ec46324effc39b95c48fb6f222055e2b33c65ae
Jefferey Cave <jefferey.cave@gmail.com>
2019-01-14 10:54:43 +0000

    Adding tabbed view

---
+++ scripts/widgets/tabbedPanel.js
@@ -0,0 +1,116 @@
+'use strict';
+export {
+	psTabbedPanelElement
+};
+
+import psPanelElement from './panel.js';
+
+export default class psTabbedPanelElement extends HTMLElement {
+	constructor() {
+		super();
+
+		let shadow = this.attachShadow({mode: 'open'});
+
+		let style= document.createElement('style');
+		shadow.append(style);
+		style.textContent = this.initialCSS;
+
+
+		let menu = document.createElement('ul');
+		shadow.append(menu);
+
+		let slot = document.createElement('slot');
+		shadow.append(slot);
+
+		this.tabs = {};
+		this.panels = {};
+		this.keys = [];
+		Array.from(this.children).forEach((d,i)=>{
+			if(!(d instanceof psPanelElement)){
+				d.style.display = 'none';
+				return;
+			}
+			d.restore();
+
+			let li = document.createElement('li');
+			li.innerHTML = d.summary;
+			menu.append(li);
+
+			let key = [i.toString(), d.id].join();
+			key = key.toLowerCase();
+			this.tabs[key] = li;
+			this.panels[key] = d;
+			this.keys.push(key);
+
+			this.tabs[key].addEventListener('click',tab=>{
+				this.activate(key);
+			});
+			this.activate(key);
+		});
+		this.activate();
+	}
+
+	activate(panel){
+		panel = panel || Object.keys(this.panels)[0];
+		panel = panel.toLowerCase();
+		for(let p of this.keys){
+			if(p === panel){
+				this.panels[p].classList.add('active');
+				this.tabs[p].classList.add('active');
+				this.panels[p].style.display = 'block';
+			}
+			else{
+				this.panels[p].classList.remove('active');
+				this.tabs[p].classList.remove('active');
+				this.panels[p].style.display = 'none';
+				this.panels[p].restore();
+			}
+		}
+	}
+
+	get initialCSS(){
+		return `
+@charset 'utf-8';
+:host > * {
+	/* display:none; */
+}
+:host > .active{
+	display:block;
+}
+:host > ul {
+	display:block;
+	padding-left:0;
+	margin-top:0;
+}
+:host > ul > li{
+	display: inline-block;
+	text-align: center;
+	padding-left:0.5em;
+	padding-right:0.5em;
+	border-bottom: 0.3em solid transparent;
+	/*
+	height:1cm;
+	*/
+	width:1cm;
+}
+:host > ul > li > span{
+	font-size: 2em;
+}
+:host > ul > li > h1{
+	font-size: 50%;
+}
+:host > ul > li.active{
+	border-bottom-color: orange;
+	border-bottom-color: var(--main-highlight);
+}
+		`;
+	}
+}
+
+
+
+window.customElements.define('ps-tabpanel',psTabbedPanelElement);
+/* global Vue */
+if(Vue && !Vue.config.ignoredElements.includes('ps-tabpanel')){
+	Vue.config.ignoredElements.push('ps-tabpanel');
+}
