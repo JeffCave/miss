930c6fb80e17bfa34531bdba31648d4e9bc6c6f0
Jeff Cave <jefferey.cave@gmail.com>
2019-11-26 20:02:22 -0400

    Basic Tests for feature [#27]

---
+++ test/algoSelection.test.js
@@ -0,0 +1,9 @@
+if(!global.Browser){
+	require('./helper');
+}
+
+describe('Algorithm Selection', function() {
+	it('all algos listed');
+	it('algo default is CPU');
+	it('GPU blocked on Firefox');
+});
